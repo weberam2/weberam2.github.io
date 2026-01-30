023cacb267b6e102180d615fdb31b0f9790f4e35
weberam2 <weberam2@gmail.com>
2026-01-29 16:08:04 -0800

    ok, small type with the date

--- renderapp.R
+++ renderapp.R
@@ -3,3 +3,8 @@ rmarkdown::render_site()
 #shinylive::export(appdir = "appfiles", destdir = "docs")
 #file.rename(from = "docs/index.html", to = "docs/futureclimbs.html")
 #file.rename(from = "docs/tmp", to = "docs/index.html")
+
+post_files <- list.files("blog_posts", pattern = "*.Rmd", full.names = TRUE)
+for(f in post_files) {
+  rmarkdown::render(f, output_dir = "docs/blog_posts")
+}
